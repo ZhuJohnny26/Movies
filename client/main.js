82b4c9762b44b2d52d1982b0b82d0040794fbf75
ZhuJohnny26 <44298471+ZhuJohnny26@users.noreply.github.com>
2021-08-17 11:04:50 -0400

    search and api functionality

--- client/main.js
+++ client/main.js
@@ -1,29 +1,43 @@
-import React, {useState} from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-
+import Secret from '../secret'
+import axios from 'axios'
 
 class Component extends React.Component{
     constructor(){
         super()
         this.state = {
             input:'',
-            movies:[]
+            movies:[],
+            var: {}
         }
         this.handleChange = this.handleChange.bind(this)
         this.get = this.get.bind(this)
+        this.search = this.search.bind(this)
     }
     async handleChange(event){
             await this.setState({[event.target.name] : event.target.value})
     }
     get() {
         console.log(this.state)
+        console.log(Secret)
+    }
+    async search(){
+        let result = await axios(`http://www.omdbapi.com/?apikey=${Secret}&s=` + this.state.input)
+        await this.setState({movies: result.data.Search})
+        
     }
     render(){
         return (
             <div>
                 test
                 <input name='input' onChange={this.handleChange}></input>
-                <button onClick={this.get}>search</button>
+                <button onClick={this.search}>search</button>
+                <button onClick={this.get} >test</button>
+                {this.state.movies.length !== 0 && 
+                    <div> wow 
+                    
+                    </div>}
             </div>
         )
     }
