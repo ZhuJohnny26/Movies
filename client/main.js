75392db9c0e2801edfe4e69b2db4f0ecbd3f9e63
ZhuJohnny26 <44298471+ZhuJohnny26@users.noreply.github.com>
2021-08-17 10:33:35 -0400

    search component

--- client/main.js
+++ client/main.js
@@ -1,67 +1,37 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
-let updating = false
-function secondFunction(){
-    console.log('Second function called');
-  }
-function firstFunction(test){
-    // Simulate a code delay
-    setTimeout( function(){
-      console.log('First function called');
-      test();
-    }, 1000 );
-  }
-  
-  firstFunction(secondFunction);
-  
-  
 
-const Component = (props) => {
-    if(!props.input){
-        let date = new Date()
-        console.log('test')
-        const [state, updateState] = useState({date: date.toDateString(), time: date.toLocaleTimeString()})
-        if(!updating){
-            updating = true
-            setInterval(function(){
-                let nextDate = new Date()
-                updateState({date: nextDate.toDateString(), time: nextDate.toLocaleTimeString()})
-            }, 1000)
+
+class Component extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            input:'',
+            movies:[]
         }
-        
+        this.handleChange = this.handleChange.bind(this)
+        this.get = this.get.bind(this)
+    }
+    async handleChange(event){
+            await this.setState({[event.target.name] : event.target.value})
+    }
+    get() {
+        console.log(this.state)
+    }
+    render(){
         return (
             <div>
-                <p>{state.time}</p>
-                <p>{state.date}</p>  
+                test
+                <input name='input' onChange={this.handleChange}></input>
+                <button onClick={this.get}>search</button>
             </div>
         )
     }
-    else if(Array.isArray(props.input)){
-        return (
-            <div>
-                <ul>
-                    {props.input.map((element, index) => (
-                        <div key={index}>{element}</div>
-                    ))}
-                </ul>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div>
-                {props.input}
-            </div>
-        )
-    }
-    
-} 
-
+}
 const App = () => (
     <div>
-    <Component input={''}/>
-    <Component input={['Componentlex', 'orange', 'grape', 'watermelon']}/>
-    <Component input={14}/>
+        test
+    <Component/>
     </div>
 )
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: './client/main.js',
+    entry: ['babel-polyfill','./client/main.js',],
     output: {
       path: __dirname,
       filename: './public/bundle.js'
